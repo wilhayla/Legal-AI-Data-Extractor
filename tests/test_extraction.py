8722aa8cba32f5c6d09b99b95dbf927cbfac4776
wilhayla <wilhayla@hotmail.com>
2026-04-22 15:14:17 -0300

    Adding fuction to verify that the test fails if the file is missing

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -2,3 +2,8 @@ import pytest
 from pathlib import Path
 from src.extraction import extract_text_from_pdf
 
+# Test 1: verify that the test fails if file is missing
+def test_file_not_found():
+    result = extract_text_from_pdf("false_file.pdf", "output.txt")
+    assert result is None
+

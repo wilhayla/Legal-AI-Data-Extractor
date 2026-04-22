4a5acb8b4586b67e0d985f0ced793a01ddb57158
wilhayla <wilhayla@hotmail.com>
2026-04-22 10:38:22 -0300

    confirming if the file was loaded

--- src/extraction.py
+++ src/extraction.py
@@ -3,4 +3,7 @@ from pathlib import Path
 
 def extract_text_from_pdf():
     pdf_file = Path("title_deed.pdf")
-    
+    print(f"DEBUG: The file the program is trying to read is: {pdf_file}")
+
+extract_text_from_pdf()
+

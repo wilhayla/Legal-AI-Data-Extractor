ec1a64e7e33d8ac27d804f1ce48de1a41020f8c7
wilhayla <wilhayla@hotmail.com>
2026-04-22 10:55:40 -0300

    if the file is not found - add check for missing PDF file

--- src/extraction.py
+++ src/extraction.py
@@ -5,5 +5,10 @@ def extract_text_from_pdf():
     pdf_file = Path("title_deed.pdf")
     print(f"DEBUG: The file the program is trying to read is: {pdf_file}")
 
+    if not pdf_file.is_file():
+        print(f"Error: The file {pdf_file} is not valid.")
+        return None
+
+
 extract_text_from_pdf()
 

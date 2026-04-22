231254c0c537fefe0991af6b6ac99b3d30a9bacb
wilhayla <wilhayla@hotmail.com>
2026-04-22 11:33:35 -0300

    Adding try-exception block for exception handling

--- src/extraction.py
+++ src/extraction.py
@@ -1,14 +1,28 @@
 import pymupdf
 from pathlib import Path
 
-def extract_text_from_pdf():
-    pdf_file = Path("title_deed.pdf")
+def extract_text_from_pdf(path_file, output_file):
+    pdf_file = Path(path_file)
     print(f"DEBUG: The file the program is trying to read is: {pdf_file}")
 
     if not pdf_file.is_file():
         print(f"Error: The file {pdf_file} is not valid.")
         return None
+    
+    try:
+        
+            
+    
+    except FileNotFoundError:
+        print(f"Error: File not found in {path_file}")
+        return False
+    
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return False
+            
 
 
-extract_text_from_pdf()
+
+
 

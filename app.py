72048c3a2dd45eb71599314ea170fd4fbe33757b
wilhayla <wilhayla@hotmail.com>
2026-04-23 16:05:34 -0300

    Set up ArgumentParser CLI entry point and tool description

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ from pathlib import Path
 from src.extraction import extract_text_from_pdf
 
 def main():
-    
+    parser = argparse.ArgumentParser(description="Legal-AI-Data_Extractor: Tool to extract text from legal PDF file")
+

16c9fd7e54e8c087dd6897e248435bb443d14f07
wilhayla <wilhayla@hotmail.com>
2026-04-23 16:15:59 -0300

    Define the arguments the user must enter

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ from src.extraction import extract_text_from_pdf
 def main():
     parser = argparse.ArgumentParser(description="Legal-AI-Data_Extractor: Tool to extract text from legal PDF file")
 
+    # Define the arguments the user must enter
+    parser.add_argument("input_pdf", type=str, help="Input PDF file path")
+    parser.add_argument("output_txt", type=str, help="Output TXT file path")

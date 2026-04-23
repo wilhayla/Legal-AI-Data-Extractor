f0a7a1b467f3591bdfd99a14a65361ee223c248f
wilhayla <wilhayla@hotmail.com>
2026-04-23 09:38:44 -0300

    Add fuction to - Test behavior with a real file - Test empty page o with short characters

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -1,4 +1,5 @@
 import pytest
+import pymupdf
 from pathlib import Path
 from src.extraction import extract_text_from_pdf
 
@@ -6,4 +7,23 @@ from src.extraction import extract_text_from_pdf
 def test_file_not_found():
     result = extract_text_from_pdf("false_file.pdf", "output.txt")
     assert result is None
+
+# Test 2: Verify behavior with a real file
+def test_succeful_extraction():
+    pdf_input = Path("data/inputs/title_deed.pdf")
+    output = Path("data/outputs/output_test.txt")
+
+    if pdf_input.exists():
+        result = extract_text_from_pdf(pdf_input, output)
+        assert result is True
+
+        if output.exists():
+            output.unlink()
+    else:
+        pytest.skip("Test file not found, skipping test.")
+
+# Test 3: Testing an empty page or with short characters.
+def test_emty_or_short_pdf():
+
+
 

998b12847ea232196bb940c16118281dfe80280c
William Haywood <wilhayla@hotmail.com>
2026-05-11 16:22:55 -0300

    feat: function to orchestrates cleaning procees

--- src/process_legal_doc.py
+++ src/process_legal_doc.py
@@ -1,2 +1,8 @@
 from src.cleaner import clean_extracted_text
 from pathlib import Path
+
+def process_document(imput_path: Path, output_path: Path):
+    '''
+    Orchestrates the cleaning procees of extracted legal text using path objects
+    '''
+    
